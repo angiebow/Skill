4bbe7087db9d8c0846cb04292203278544818ffd
Pelangi Masita <pelangimasita18@gmail.com>
2024-11-26 17:09:22 +0700

    navbar route

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
 
 export const NavbarRoutes = () => {
+  const pathname = usePathname();
+
   return (
     <div className="flex gap-x-2 ml-auto">
       <UserButton />
