12a699a217414d07184c64bcf99081ba664eb0c5
Pelangi Masita <pelangimasita18@gmail.com>
2024-11-29 09:47:57 +0700

    prisma

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -4,7 +4,9 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import toast from "react-hot-toast";
 
 import {
   Form,
@@ -12,10 +14,12 @@ import {
   FormDescription,
   FormField,
   FormLabel,
-  FormMessage
+  FormMessage,
+  FormItem,
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Ghost } from "lucide-react";
 
 const formSchema = z.object({
   title: z.string().min(1, {
@@ -24,6 +28,7 @@ const formSchema = z.object({
 });
 
 const CreatePage = () => {
+  const router = useRouter();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -33,13 +38,70 @@ const CreatePage = () => {
 
   const { isSubmitting, isValid } = form.formState;
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post("/api/course", values);
+      router.push(`/teacher/create/${response.data.id}`);
+    } catch {
+      toast.error("An error occurred. Please try again.");
+    }
   }
 
   return ( 
-    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center">
-      Create courses
+    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+      <div>
+        <h1 className="text-2xl">
+          Name your course
+        </h1>
+        <p className="text-sm text-slate-600">
+          What would you like to name your course? Don't worry you can change this later.
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y- mt-8"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Course Title
+                  </FormLabel>
+                  <FormControl>
+                    <Input 
+                      disabled={isSubmitting}
+                      placeholder="ex: Data Structures and Algorithms"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    What will you teach in this course?
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Link href="/">
+                <Button 
+                  type="button"
+                  variant="ghost"
+                >
+                  Cancel
+                </Button>
+              </Link>
+              <Button
+                type="submit"
+                disabled={!isValid || isSubmitting}
+              >
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </div>
    );
 }
