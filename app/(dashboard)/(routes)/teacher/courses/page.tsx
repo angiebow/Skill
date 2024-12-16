c3053dc7ad03bcf9f4cde776d984c84c1c5422b4
Bimantara Putra Ernandra <bima.pe02@gmail.com>
2024-12-16 13:31:31 +0700

    courses list

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,55 +1,42 @@
-"use client";
+//"use client";
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useUser } from "@clerk/nextjs";
-import { useState, useEffect } from "react";
+import { DataTable } from "./_components/data-table";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
+import { columns } from "./_components/columns";
 
-const CoursesPage = () => {
-  const { user } = useUser(); // Get user information from Clerk
-  const [showWarning, setShowWarning] = useState(false);
+const CoursesPage = async () => {
+  const { userId }: { userId: string | null } = await auth();
+  if (!userId) {
+      return redirect("/");
+  }
 
-  // Check user status and trigger warning only when the component mounts
-  useEffect(() => {
-    if (!user) {
-      setShowWarning(true); // Show warning if not logged in
-    }
-  }, [user]);
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  
+  //const data = await getData();
+  //const [showWarning, setShowWarning] = useState(false);
 
   return (
     <div>
-      {/* Show warning if user is not logged in */}
-      {showWarning && !user && (
-        <div className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-white p-6 rounded-md shadow-md text-center">
-            <p>Please sign up or sign in first to access Teacher Mode</p>
-            <div className="mt-4">
-              {/* Sign In Button */}
-              <Link href="/sign-in">
-                <Button size="sm" variant="outline" className="w-full">
-                  Sign In
-                </Button>
-              </Link>
-            </div>
-          </div>
-        </div>
-      )}
-
-      {/* Page content (disabled interaction if not logged in) */}
-      <div className={`relative ${showWarning && !user ? "pointer-events-none" : ""}`}>
-        {/* Content of the page */}
-        {user ? (
-          <div>
+        <div>
+            <DataTable columns={columns} data={courses} />
             <Link href="/teacher/create">
               <Button className="p-6">
                 New course
               </Button>
             </Link>
-          </div>
-        ) : (
-          <div>Loading...</div>
-        )}
-      </div>
+        </div>
     </div>
   );
 };
